e2bd26fc225e4e2142f99a93c76f9b75ae56e195
Nikolai Kavtur <nikolaikavtur@coherentsolutions.com>
2021-07-16 12:10:51 +0300

    121: Best Time to Buy and Sell Stock

---
+++ src/121.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices):
+        min_price, best_diff = float("+infinity"), 0
+
+        for current_price in prices:
+            if current_price < min_price:
+                max_price = min_price = current_price
+            elif current_price > max_price:
+                max_price = current_price
+
+            best_diff = max(best_diff, max_price - min_price)
+
+        return best_diff
+
+
+Solution().maxProfit([7, 1, 5, 3, 6, 4])
