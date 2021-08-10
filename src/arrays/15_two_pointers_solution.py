a411facc27267a170c3df659c3cfa1140be15e40
n.kavtur <n.kavtur@gmail.com>
2021-08-10 12:02:52 +0300

    15: two pointers solution

---
+++ src/arrays/15_two_pointers_solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums):
+        sorted_nums = sorted(nums)
+
+        triplets = set()
+        for i in range(len(sorted_nums) - 1):
+            self._two_sum(i, sorted_nums, triplets)
+
+        return triplets
+
+    def _two_sum(self, first_val_index, sorted_nums, triplets):
+        low, high = first_val_index + 1, len(sorted_nums) - 1
+
+        first_value = sorted_nums[first_val_index]
+        while low != high:
+
+            if sorted_nums[low] + sorted_nums[high] + first_value > 0:
+                high -= 1
+            elif sorted_nums[low] + sorted_nums[high] + first_value < 0:
+                low += 1
+            else:
+                triplets.add((first_value, sorted_nums[low], sorted_nums[high]))
+                low += 1
+
+
+# print(Solution().threeSum(nums=[-1, 0, 1, 2, -1, -4]))
+# print(Solution().threeSum(nums=[]))
+print(Solution().threeSum(nums=[-2, 0, 1, 1, 2]))
